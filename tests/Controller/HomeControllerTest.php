d365db74e362b15bc8ee3f0e93b31b9d90d4e82f
Doudou <adeldoudou1996@gmail.com>
2020-02-05 21:09:45 +0100

    p8

--- tests/Controller/HomeControllerTest.php
+++ tests/Controller/HomeControllerTest.php
@@ -13,11 +13,30 @@ class HomeControllerTest extends WebTestCase
         $this->assertSame(200, $client->getResponse()->getStatusCode());
     }
 
-    public function testAddNewTask()
+    public function testHomePage()
     {
-        /* $client = static::createClient();
-        $crawler = $client->request('GET', '/tasks/create');
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
 
-        $form = $crawler->selectButton('Ajouter')->form(); */
+        $this->assertSame(1, $crawler->filter('html:contains("Bienvenue sur Todo List")')->count());
     }
+
+    public function testAddNewUser()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/users/create');
+
+        $form = $crawler->selectButton('Ajouter')->form(); 
+
+        $form['user[username]'] = 'AdYehh';
+        $form['user[email]'] = 'emailName';
+        $form['user[password]'] = 'password';
+        $form['user[roles]'] = 'role_admin';
+
+        $client->submit($form);
+
+        $client->followRedirect();
+
+        echo $client->getResponse()->getContent();
+    }  
 }
