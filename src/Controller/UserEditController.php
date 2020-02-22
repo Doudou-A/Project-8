d677230aeb1e5a1c78c702496a5029d8dd8a6871
Doudou <adeldoudou1996@gmail.com>
2020-02-22 18:35:58 +0100

    p8

--- src/Controller/UserEditController.php
+++ src/Controller/UserEditController.php
@@ -21,7 +21,7 @@ class UserEditController extends AbstractController
 
             $userManager->edit($user);
 
-            $this->addFlash('success', "L'utilisateur a bien été modifié");
+            $this->addFlash('success', "Utilisateur modifié avec succès !");
 
             return $this->redirectToRoute('user_list');
         }
